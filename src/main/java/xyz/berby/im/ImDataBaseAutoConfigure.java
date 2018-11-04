0262b0759515e5614f0047df0dfb67337c703123
743834110 <743834110@qq.com>
2018-11-04 23:54:25 +0800

    2018/11/4

--- src/main/java/xyz/berby/im/ImDataBaseAutoConfigure.java
+++ src/main/java/xyz/berby/im/ImDataBaseAutoConfigure.java
@@ -1,7 +1,9 @@
 package xyz.berby.im;
 
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.ApplicationContext;
@@ -11,15 +13,18 @@ import xyz.berby.im.util.ApplicationContextHolder;
 
 import javax.annotation.PostConstruct;
 
-@EnableAutoConfiguration(exclude = ImDataBaseAutoConfigure.class)
+//@EnableAutoConfiguration(exclude = ImDataBaseAutoConfigure.class)
+@Configuration
 @PropertySource("classpath:application.properties")
+@ComponentScan(value = {"xyz.berby.im.controller", "xyz.berby.im.service"})
+@MapperScan("xyz.berby.im.dao")
+@AutoConfigureBefore
 public class ImDataBaseAutoConfigure {
 
     @Autowired
-    private ImDataBaseAutoConfigure(ApplicationContext applicationContext) {
+    public ImDataBaseAutoConfigure(ApplicationContext applicationContext) {
         ApplicationContextHolder contextHolder = new ApplicationContextHolder();
         contextHolder.setApplicationContext(applicationContext);
-        System.out.println("fffffffffffff" + applicationContext);
     }
 
     public static void main(String [] args) {
